2088f2ceaa93f8c7575eeb9ed6c31245519a5d03
Marc Riddick Verano <marc_riddick_verano@ligph.com>
2022-03-09 18:47:59 +0800

    Update database config for deployment setup

--- database.js
+++ database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: 5433,
-  password: "",
-  database: "postgres"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "postgres",
+  port: process.env.DB_PORT || 5433,
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "postgres"
 })
 
 module.exports = pool;
