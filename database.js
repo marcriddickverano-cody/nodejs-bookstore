accd0263a60383fcba1e05d7d20d6ac2752f8a2e
Marc Riddick Verano <marc_riddick_verano@ligph.com>
2022-03-09 19:31:47 +0800

    Fix error

--- database.js
+++ database.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool
 
+let pool = new Pool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "postgres",
+  port: process.env.DB_PORT || 5433,
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "postgres"
+});
+
 if (process.env.DATABASE_URL) {
-  const pool = new Pool({
+  pool = new Pool({
     connectionString: process.env.DATABASE_URL
-  });
-} else {
-  const pool = new Pool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "postgres",
-    port: process.env.DB_PORT || 5433,
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "postgres"
   });
 }
 
