26f9b8b772ea655f1fbb26dea243bbf3ee97c114
Marc Riddick Verano <marc_riddick_verano@ligph.com>
2022-03-09 19:29:33 +0800

    Add new database connection method.

--- database.js
+++ database.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool
 
-const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "postgres",
-  port: process.env.DB_PORT || 5433,
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "postgres"
-})
+if (process.env.DATABASE_URL) {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+  });
+} else {
+  const pool = new Pool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "postgres",
+    port: process.env.DB_PORT || 5433,
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "postgres"
+  });
+}
 
 module.exports = pool;
